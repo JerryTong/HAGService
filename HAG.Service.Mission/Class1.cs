5180cde9b74e3a9b9e0bca5346583870ecfb91fa
JerryTong <a866559@gmail.com>
2016-10-15 09:48:59 +0800

    ADD Service

---
+++ HAG.Service.Mission/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAG.Service.Mission
+{
+    public class Class1
+    {
+    }
+}
