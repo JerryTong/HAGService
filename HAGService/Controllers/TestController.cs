aa02a896ae713d70f72594081ca54652ffb781fd
JerryTong <a866559@gmail.com>
2016-10-18 00:04:19 +0800

    add test service

---
+++ HAGService/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HAGService.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/test/test")]
+        public string Test()
+        {
+            return "Service is okay.";
+        }
+    }
+}
